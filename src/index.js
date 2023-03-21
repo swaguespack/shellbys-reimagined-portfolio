d07e7d3e32e4aac066d9c6ec76b0c4b4112f6723
shellby.miller <shellby.miller@gmail.com>
2023-03-21 19:34:59 -0400

    added code to index.js in src to import React and render components to DOM from root

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./index.css";
 
+// Render commponents into DOM from id=root
 ReactDOM.render(
   <React.StrictMode>
     <App />
